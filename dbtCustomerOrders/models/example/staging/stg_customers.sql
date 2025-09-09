4fe7eef61181d4764ba2fbac2b58d32845112761
NOEL S. DAVID <noelsdavid@gmail.com>
2025-09-09 03:00:31 +0000

    Models and Docs

---
+++ dbtCustomerOrders/models/example/staging/stg_customers.sql
@@ -0,0 +1,8 @@
+with customers as (
+    select
+        customer_id,
+        customer_name,
+        city
+    from {{ source('raw', 'customers') }}
+)
+select * from customers
---
+++ dbtCustomerOrders/models/example/staging/stg_orders.sql
@@ -0,0 +1,9 @@
+with orders as (
+    select
+        order_id,
+        customer_id,
+        order_date,
+        total_amount
+    from {{ source('raw', 'orders') }}
+)
+select * from orders
